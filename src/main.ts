49c8081556aab2bf9954b90e08ec416d322a38c0
Nguyen Tam Thanh <48196420+ThanhDeveloper@users.noreply.github.com>
2022-04-17 20:57:57 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidateAuthMiddleware } from './core/middlewares/validate-auth.middleware';
 import { setupSwagger } from './open-api';
+require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
+  if(process.env.NODE_ENV !== 'production'){
+    setupSwagger(app);
+  }
   app.useGlobalPipes(new ValidateAuthMiddleware());
   await app.listen(5001);
 }
